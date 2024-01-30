7924c805c375ec5eca9180a21735f717dabd52a6
okkyariatnavaleri <okkyaryadna97@gmail.com>
2024-01-30 23:36:47 +0700

    comment

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'level' => 'admin'
+            ],
+            [
+                'name' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('user123'),
+                'level' => 'user'
+            ],
+            [
+                'name' => 'okky',
+                'email' => 'okky@gmail.com',
+                'password' => Hash::make('okky123'),
+                'level' => 'admin'
+            ]
+        ]);
+    }
+}
